ff517fba33676a2fed6d8fd7bfbf164859526fe0
rozikin <khoiurrozikin86@gmail.cm>
2021-11-19 13:26:17 +0700

    coba array

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,11 +8,20 @@ class Pages extends BaseController
 {
     public function index()
     {
-        return view('pages/home');
+        $data = [
+            'title' => "Home | Kurios Utama",
+            'tes'   => ['satu', 'dua', 'tiga', 'empat']
+        ];
+
+        echo view('layout/header', $data);
+        echo view('pages/home');
+        echo view('layout/footer');
     }
 
     public function about()
     {
-        return view('pages/about');
+        echo view('layout/header');
+        echo view('pages/about');
+        echo view('layout/footer');
     }
 }
